92b481d217d9b358b214f203cff4d21c22eed729
sarafkalpak <70629954+sarafkalpak@users.noreply.github.com>
2020-11-28 19:35:24 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -26,7 +26,7 @@ const title = 'Select the gradient and then the Box to change the color';
 const App = () => {
   let [nextBackground, selectNextBackground] = useState({ nextBackground: "" })
   const applyColor = (updateSelectionStyle) => {
-    updateSelectionStyle(nextBackground)
+    updateSelectionStyle(nextBackground);
   }
 
 
